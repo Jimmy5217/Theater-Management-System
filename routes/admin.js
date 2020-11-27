dc749def53dcbcce933075045eadb72d62c93b21
裴子华 <peizihua@peizihuadeMacBook-Pro.local>
2020-11-27 12:24:27 +0800

    update movie delete movie search movie

--- routes/admin.js
+++ routes/admin.js
@@ -73,5 +73,81 @@ router.post('/movie', async (req, res) => {
   });
 
 
+  router.delete('/:id', async (req, res) => {
+    if (!req.params.id) throw 'You must specify an ID to delete';
+    try {
+      await moviesData.get(req.params.id);
+    } catch (e) {
+      res.status(404).json({ error: 'movie not found' });
+      return;
+    }
+  
+    try {
+      await moviesData.remove(req.params.id);
+      res.json({"movieId": req.params.id, "deleted": true});
+    } catch (e) {
+      res.sendStatus(500);
+    }
+  });
+
+
+  router.patch('/:id', async (req, res) => {
+    const requestBody = req.body;
+    let updatedObject = {};
+    try {
+      const oldmovie = await moviesData.get(req.params.id);
+      console.dir(oldmovie);
+      if (requestBody.moviename && requestBody.moviename !== oldmovie.moviename)
+        updatedObject.moviename = requestBody.moviename;
+      if (requestBody.cast && requestBody.cast !== oldmovie.cast)
+        updatedObject.cast = requestBody.cast;
+      if (requestBody.genre && requestBody.genre !== oldmovie.genre)
+        updatedObject.genre = requestBody.genre;
+      if (requestBody.runtime && requestBody.runtime !== oldmovie.runtime)
+        updatedObject.runtime = requestBody.runtime; 
+      if (requestBody.plot && requestBody.plot !== oldmovie.plot)
+        updatedObject.plot = requestBody.plot;
+      if (requestBody.rating && requestBody.rating !== oldmovie.rating)
+        updatedObject.rating = requestBody.rating;
+      if (requestBody.releaseTime && requestBody.releaseTime !== oldmovie.releaseTime)
+        updatedObject.releaseTime = requestBody.releaseTime;
+      if (requestBody.selledTicket && requestBody.selledTicket !== oldmovie.selledTicket)
+        updatedObject.selledTicket = requestBody.selledTicket;
+      if (requestBody.image && requestBody.image !== oldmovie.image)
+        updatedObject.image = requestBody.image;
+      if (requestBody.id && requestBody.id !== oldmovie.id)
+        updatedObject.id = requestBody.id;
+    } catch (e) {
+      res.status(404).json({ error: 'movie not found' });
+      return;
+    }
+    //console.dir(updatedObject);
+    if (Object.keys(updatedObject).length !== 0) {
+      try {
+        const updatedmovie = await moviesData.updatemovie(
+          req.params.id,
+          updatedObject
+        );
+        res.json(updatedmovie);
+      } catch (e) {
+        res.status(500).json({ error: e });
+      }
+    } else {
+      
+      res.status(400).json({error:'No fields have been changed from their inital values, so no update has occurred'});
+    }
+  });
+
+  router.get('/:name', async (req, res) => {
+    try {
+      let movie = await moviesData.getmoviebyname(req.params.name);
+      console.dir(movie);
+      res.json(movie);
+    } catch (e) {
+      res.status(404).json({ error: 'User not found' });
+    }
+  });
+
+
 
   module.exports = router;
